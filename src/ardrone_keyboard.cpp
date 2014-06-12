ba68de1451a3f3f3a8ce4c638859532ed5a1352a
Reem <reem.ashour@kustar.ac.ae>
2014-06-12 20:40:45 +0400

    added some comments and modifecations. NOTE: The reset button doesn't hold. I toggle again to the original situation. It needs to be fixed.

--- src/ardrone_keyboard.cpp
+++ src/ardrone_keyboard.cpp
@@ -1,35 +1,27 @@
 /*
- * Copyright (c) 2011, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+* Copyright (C) 2013 - 2014 by                                             *
+* reem ashour, Khalifa University Robotics Institute KURI               *
+* <reem.ashour@kustar.ac.ae>                                          *
+*                                                                          *
+* 									   *
+* This program is free software; you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published by     *
+* the Free Software Foundation; either version 2 of the License, or        *
+* (at your option) any later version. 					   *
+* 									   *
+* This program is distributed in the hope that it will be useful, 	   *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 	   *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 		   *
+* GNU General Public License for more details. 				   *
+* 									   *
+* You should have received a copy of the GNU General Public License 	   *
+* along with this program; if not, write to the 			   *
+* Free Software Foundation, Inc., 					   *
+* 51 Franklin Steet, Fifth Floor, Boston, MA 02111-1307, USA. 		   *
  */
 
 #include <ros/ros.h>
 #include <signal.h>
-
 #include <std_msgs/Empty.h>
 #include <std_srvs/Empty.h>
 #include<stdio.h>
@@ -65,10 +57,11 @@ int main(int argc, char** argv)
     while (ros::ok())
     {
         int c = getch();   // call your non-blocking input function
-        if (c == 't')
+        if (c == 't' || c="T") // take off
         {
             std::cout << "takeoff" << std::endl ;
             double time_start=(double)ros::Time::now().toSec();
+            // flat trim before taking off
             flat_trim_client.call(empty);
             flat_trim_client.call(empty);
             while ((double)ros::Time::now().toSec()< time_start+5.0) /* Send command for five seconds*/
@@ -81,7 +74,7 @@ int main(int argc, char** argv)
 
         }
 
-        else if (c == 'l')
+        else if (c == 'l' || c=="L") // landing
         {
             std::cout << "land" << std::endl ;
 
@@ -95,7 +88,7 @@ int main(int argc, char** argv)
             ROS_INFO("ARdrone landed");
             exit(0);
         }
-        else if (c == 'e')
+        else if (c == 'r' || c =="R") // reset
         {
             std::cout << "emergency" << std::endl ;
 
@@ -110,3 +103,6 @@ int main(int argc, char** argv)
     }
 
 }
+
+
+// http://answers.ros.org/question/63491/keyboard-key-pressed/
