d33431b537e77cea47f8f47db788ff104401d318
reem90 <reem.ashour@kustar.ac.ae>
2015-02-22 10:18:50 +0400

    Added the collision detection class

--- src/CollisionDetection.cpp
+++ src/CollisionDetection.cpp
@@ -1,127 +1,19 @@
-#include "ros/ros.h"
-#include "tf/transform_listener.h"
-#include "sensor_msgs/PointCloud.h"
-#include "sensor_msgs/PointCloud2.h"
-#include "tf/message_filter.h"
-#include "message_filters/subscriber.h"
-#include "laser_geometry/laser_geometry.h"
-#include <octomap_msgs/Octomap.h>
-#include <octomap/ColorOcTree.h>
-#include <octomap/octomap.h>
-#include <octomap/OcTree.h>
-#include "nav_msgs/Odometry.h"
-
-#include <iostream>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-//#include <pcl/conversions.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/PCLPointCloud2.h>
-#include <pcl/filters/filter.h>
-#include <octomap_msgs/conversions.h>
-#include <deque>
-//#include <ghmm/GHMM.hpp>
-//#include <haptic_teleoperation/Num.h>
-#include <haptic_teleoperation/hmm_srv.h>
-#include <visualization_msgs/MarkerArray.h>
-#include <std_msgs/Bool.h>
-
-#include "fcl/shape/geometric_shapes.h"
-#include "fcl/narrowphase/narrowphase.h"
-#include "fcl/collision.h"
-#include "fcl/ccd/motion.h"
-#include <stdlib.h>
-#include <boost/foreach.hpp>
-#include <Eigen/Eigen>
-#include "fcl/octree.h"
-#include "fcl/traversal/traversal_node_octree.h"
-#include "fcl/broadphase/broadphase.h"
-#include "fcl/shape/geometric_shape_to_BVH_model.h"
-#include "fcl/math/transform.h"
-
-#include <boost/filesystem.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/array.hpp>
-#include <octomap/octomap.h>
-#include "fcl/BV/AABB.h"
-#include "fcl/collision_object.h"
-//#include <moveit_visual_tools/moveit_visual_tools.h>
+#include "haptic_teleoperation/CollisionDetection.h"
 
 
-
-using namespace std;
-using namespace fcl;
-//using namespace octomap;
-
-Eigen::Vector3d robotpose ;
-double poseQ[4] ;
-
-FCL_REAL extents [6] = {0, 0, 0, 10, 10, 10};
-GJKSolver_libccd solver1;
-GJKSolver_indep solver2;
-//#define BOOST_CHECK_FALSE(p) BOOST_CHECK(!(p))
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-
-const float res = 0.1;
-std::deque< pcl::PointCloud<pcl::PointXYZ> > cloud_seq_loaded;
-
-
-
-struct TStruct
-{
-    std::vector<double> records;
-    double overall_time;
-    TStruct() { overall_time = 0; }
-    void push_back(double t)
-    {
-        records.push_back(t);
-        overall_time += t;
-    }
-};
-
-
-
-class LaserScanToPointCloud{
-public:
-
-    ros::NodeHandle n_;
-    laser_geometry::LaserProjection projector_;
-    tf::TransformListener listener_;
-    //message_filters::Subscriber<sensor_msgs::LaserScan> laser_sub_;
-    //tf::MessageFilter<sensor_msgs::LaserScan> laser_notifier_;
-    //ros::Publisher laser_pub;
-    ros::Subscriber laser_sub;
-    ros::Publisher vis_pub ;
-    ros::Publisher octmap_pub;
-    ros::Publisher collide_F_pub ;
-    ros::Subscriber slave_pose_sub ;
-    ros::Publisher  vis_cude_pub ;
-    //  moveit_visual_tools::MoveItVisualToolsPtr visual_tools_;
-
-    LaserScanToPointCloud(ros::NodeHandle n):n_(n)
+CollisionDetection::CollisionDetection(ros::NodeHandle n):n_(n)
     {
 
-        laser_sub = n_.subscribe("/scan",1, &LaserScanToPointCloud::laserCallback, this);
-        slave_pose_sub = n_.subscribe("/RosAria/pose" , 100 ,&LaserScanToPointCloud::poseCallback, this);
-
-        //laser_pub = n_.advertise<sensor_msgs::PointCloud2>("pointCloudObs", 10);
         vis_pub = n_.advertise<visualization_msgs::Marker>("Sphere", 1);
         vis_cude_pub = n_.advertise<visualization_msgs::MarkerArray>("Cube", 1);
-
         octmap_pub = n_.advertise<octomap_msgs::Octomap>("octomap_rviz", 1);
         collide_F_pub = n_.advertise<std_msgs::Bool>("collision_flag",1) ;
 
-        //      visual_tools_.reset(new moveit_visual_tools::MoveItVisualTools("base_link","/moveit_visual_markers"));
-
     }
 
-    void laserCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in)
+
+ bool CollisionDetection::inCollision(sensor_msgs::LaserScan scan_in , nav_msgs::Odometry robot_pose )
     {
-        double t1 = ros::Time::now().toSec() ;
-        //std::cout<<"LASER" << scan_in->header.frame_id << std::endl ;
         if(!listener_.waitForTransform(scan_in->header.frame_id,
                                        "base_link",
                                        // "Pioneer3AT/base_link",  // GAZEBO
@@ -134,26 +26,22 @@ public:
         sensor_msgs::PointCloud msg;
         //projector_.projectLaser(*scan_in, msg);
         projector_.transformLaserScanToPointCloud("base_link",*scan_in, msg,listener_);
-        // std::cout << "cloud_size" << msg.points.size() << std::endl;
-        // std::cout << "cloud_0" << msg.points[0].x << std::endl;
+
+
 
 
 
         octomap::OcTree* st_tree = new octomap::OcTree(0.1);
-        //     OcTree st_tree (0.01); // create empty tree with resolution 0.1
         octomap::Pointcloud st_cld;
-        // visualization_msgs::MarkerArray marker_array ; // = rviz_arrows(force_field, obstacles_positions_current, std::string("potential_field"));
         for(int i = 0;i<msg.points.size();i++){
             if((msg.points[i].x*msg.points[i].x + msg.points[i].y*msg.points[i].y + msg.points[i].z + msg.points[i].z) < 2 )
             {
                 octomap::point3d endpoint((float) msg.points[i].x,(float) msg.points[i].y,(float) msg.points[i].z);
-                //  visualization_msgs::Marker marker =  rviz_arrow(endpoint,i ,"tree");
-                //    marker_array.markers.push_back(marker);
                 st_cld.push_back(endpoint);
             }
         }
-        //visualization_markers_pub_map.publish(marker_array);
-        // maybe this should be th position of the robot
+
+
         octomap::point3d origin(0.0,0.0,0.0);
         st_tree->insertPointCloud(st_cld,origin);
         st_tree->updateInnerOccupancy();
@@ -173,19 +61,10 @@ public:
 
         std_msgs::Bool collide_flag;
         boost::shared_ptr<Box> Shpere0(new Box(1,1,1));
-        //        GJKSolver_libccd solver;
-        //        Vec3f contact_points;
-        //        FCL_REAL penetration_depth;
-        //        Vec3f normal;
-
         Transform3f tf0, tf1;
         tf0.setIdentity();
-        tf0.setTranslation(Vec3f(robotpose(0),robotpose(1),robotpose(2)));
-
+        tf0.setTranslation(Vec3f(robot_pose->pose.pose.position.x,robot_pose->pose.pose.position.y,robot_pose->pose.pose.position.z));
         tf1.setIdentity();
-
-
-
         CollisionObject co0(Shpere0, tf0);
         AABB a = co0.getAABB() ;
         Vec3f vec =  a.center() ;
@@ -197,12 +76,11 @@ public:
 
 
         visualization_msgs::MarkerArray marker_array ;
-        for(size_t i = 0; i < boxes.size(); ++i)
+        for(size_t i = 0; i <= boxes.size(); ++i)
         {
             CollisionObject* box =  boxes[i];
-            //bool res2 = solver.shapeIntersect(*Shpere0, tf0, box, tf1, &contact_points, &penetration_depth, &normal);
-            //std::cout << "res2" << res2 << std::endl ;
-            static const int num_max_contacts = std::numeric_limits<int>::max();//std::numeric_limits<int>::max();
+
+            static const int num_max_contacts = std::numeric_limits<int>::max();
             static const bool enable_contact = true ;
 
             AABB b = box->getAABB() ;
@@ -213,45 +91,30 @@ public:
             fcl::CollisionResult result;
             fcl::CollisionRequest request(num_max_contacts, enable_contact);
             fcl::collide(&co0, box, request, result);
-            //          vector<Contact> contacts;
-            //          result.getContacts(contacts);
-            //          std::cout << "size: " <<  contacts.size() << std::endl ;
+
+	    vis_cude_pub.publish(marker_array);
+            for(size_t i = 0; i < boxes.size(); ++i)
+            delete boxes[i];
+            
             if (result.isCollision() == true )
             {
-
                 std::cout << "EXIT " << std::endl ;
-                // exit(0) ;
-                collide_flag.data = true ;
-                collide_F_pub.publish(collide_flag) ;
+                return true ; 
 
             }
             else
             {
-                collide_flag.data = false ;
-                collide_F_pub.publish(collide_flag) ;
+		return false; 
             }
         }
-        vis_cude_pub.publish(marker_array);
-        for(size_t i = 0; i < boxes.size(); ++i)
-            delete boxes[i];
-        double t2 = ros::Time::now().toSec();
-        std::cout << "Operation TIme is " << t2 - t1 << std::endl ;
+        
 
-    }
-    void poseCallback(const nav_msgs::Odometry::ConstPtr & robot_pose)
-    {
-        // std::cout << "get robot data " << std::endl ;
-        robotpose(0) =  robot_pose->pose.pose.position.x ;
-        robotpose(1) =  robot_pose->pose.pose.position.y  ;
-        robotpose(2) =  robot_pose->pose.pose.position.z ;
-        poseQ[0] = robot_pose->pose.pose.orientation.x;
-        poseQ[1] = robot_pose->pose.pose.orientation.y;
-        poseQ[2] = robot_pose->pose.pose.orientation.z;
-        poseQ[3] = robot_pose->pose.pose.orientation.w;
+
     }
 
 
-    void generateBoxesFromOctomap(std::vector<CollisionObject*>& boxes, OcTree& tree)
+
+    void CollisionDetection::generateBoxesFromOctomap(std::vector<CollisionObject*>& boxes, OcTree& tree)
     {
 
         std::vector<boost::array<FCL_REAL, 6> > boxes_ = tree.toBoxes();
@@ -273,7 +136,7 @@ public:
     }
 
 
-    void drawSphere(Vec3f vec )
+    void CollisionDetection::drawSphere(Vec3f vec )
     {
         visualization_msgs::Marker marker;
         marker.header.frame_id = "odom";
@@ -306,7 +169,7 @@ public:
         vis_pub.publish( marker );
     }
 
-    visualization_msgs::Marker drawCUBE(Vec3f vec , int id )
+    visualization_msgs::Marker CollisionDetection::drawCUBE(Vec3f vec , int id )
     {
         visualization_msgs::Marker marker;
         marker.header.frame_id = "odom";
@@ -338,23 +201,3 @@ public:
         //marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
         return marker ;
     }
-
-};
-
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "my_scan_to_cloud");
-    ros::NodeHandle n;
-    ros::NodeHandle n_priv("~");
-    double freq;
-    n_priv.param<double>("frequency", freq, 100.0);
-    ros::Rate loop_rate(freq);
-    LaserScanToPointCloud lstopc(n);
-    std::cout << "Object created" << std::endl ;
-    while(ros::ok())
-    {
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-    return 0;
-}
